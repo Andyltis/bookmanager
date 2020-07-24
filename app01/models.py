e067be2c480c6cee2a406238e54595ee17cb6911
luoxu <luoxu@163.com>
2020-07-24 23:12:31 +0800

    新增项目文件

---
+++ app01/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=32)
+
+class Book(models.Model):
+    name = models.CharField(max_length=32)
+    # publisher_id = models.ForeignKey(Publisher)
+    # models.CASCADE 为级联删除
+    # models.PROTECT 保护
+    # models.SET(val) # 删除后设置为指定值
+    # models.SETDEFAULT, default=val  删除后设置为default值
+    # model.SET_NULL 删除后设置为null
+    publisher = models.ForeignKey("Publisher", on_delete=models.CASCADE)  # 默认是级联删除
---
+++ app01/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from app01 import models
+# Create your views here.
+def publisher_list(request):
+    # 返回所有出版社信息
+    all_publisher = models.Publisher.objects.all().order_by("-id")
+    # for i in all_publisher:
+    #     name = i.name
+
+    return render(request, "publisher_list.html", context={"all_publisher": all_publisher})
+
+def publisher_add(request):
+    if request.method == "POST":
+        name = request.POST.get("pub_name")
+        if not name:
+            return render(request, "publisher_add.html", {"error": "出版社名称不能为空"})
+        if models.Publisher.objects.filter(name=name):
+            return render(request, "publisher_add.html", {"error": "出版社已存在"})
+        models.Publisher.objects.create(name=name)
+        return redirect("/publisher_list")
+
+    return render(request, "publisher_add.html")
+    pass
+
+def publisher_del(request):
+    del_id = request.GET.get("id")
+    obj = models.Publisher.objects.filter(pk=del_id)
+    obj.delete()
+
+    return redirect("/publisher_list")
+
+
+def publisher_edit(request):
+    pk = request.GET.get('pk')
+    pub_obj = models.Publisher.objects.get(pk=pk)
+    if request.method == "POST":
+        pub_obj.name = request.POST.get("pub_name", "")
+        pub_obj.save()
+        return redirect("/publisher_list")
+
+    return render(request, "publisher_edit.html", {"pk": pub_obj})
